1b58fe3b342e12ff2d747eba76b3f27b444e133b
Niels Mokkenstorm <niels@mokkenstorm.dev>
2022-03-19 13:16:08 +0100

    minor config files

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  darkMode: true,
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [require("@tailwindcss/forms")],
+}
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require("path")
+
+const CleanTerminalPlugin = require("clean-terminal-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = {
+  entry: {
+    app: ["./src/index.tsx"],
+    vendor: ["react", "react-dom"],
+  },
+  devServer: {
+    compress: true,
+    port: 9000,
+    historyApiFallback: true,
+  },
+  mode: "development",
+  optimization: {
+    runtimeChunk: "single",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "js/[name].bundle.js",
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|pdf)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "ts-loader",
+      },
+      {
+        test: /\.(graphql|gql)$/,
+        exclude: /node_modules/,
+        loader: "graphql-tag/loader",
+      },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                ident: "postcss",
+                plugins: [require("tailwindcss"), require("autoprefixer")],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanTerminalPlugin(),
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+}
