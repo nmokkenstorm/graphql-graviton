b8a782f72d4f4392c0d8956bd114c1baf12b44ec
Niels Mokkenstorm <niels@mokkenstorm.dev>
2022-03-19 14:16:06 +0100

    make name optional

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -2,7 +2,7 @@ import React, { useState, ReactNode } from "react"
 
 interface TextInputProps<T = string> {
   onChange: (value: T) => void
-  name: string
+  name?: string
   placeholder?: string
   label?: string | ReactNode
   id?: string
@@ -23,12 +23,14 @@ export const TextInput = ({
 
   return (
     <div className="relative border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
-      <label
-        htmlFor={id ?? name}
-        className="absolute -top-2 left-2 -mt-px inline-block px-1 bg-white text-xs font-medium text-gray-900"
-      >
-        {label ?? name}
-      </label>
+      {label || name ? (
+        <label
+          htmlFor={id ?? name}
+          className="absolute -top-2 left-2 -mt-px inline-block px-1 bg-white text-xs font-medium text-gray-900"
+        >
+          {label ?? name}
+        </label>
+      ) : null}
       <input
         autoFocus={autoFocus}
         type="text"
