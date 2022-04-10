531df67a6f2abbd32a590256e6476f46774028c7
Niels Mokkenstorm <niels@mokkenstorm.dev>
2022-04-10 17:52:17 +0200

    feat: add empty state component

---
+++ packages/components/lib/EmptyState.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+interface EmptyStateProps {
+  title: string
+  subtitle: string
+}
+
+export const EmptyState = ({ title, subtitle }: EmptyStateProps) => (
+  <>
+    <svg
+      className="mx-auto h-12 w-12 text-gray-400"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      aria-hidden="true"
+    >
+      <path
+        vector-effect="non-scaling-stroke"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z"
+      />
+    </svg>
+    <h3 className="mt-2 text-sm font-medium text-gray-900">{title}</h3>
+    <p className="mt-1 text-sm text-gray-500">{subtitle}</p>
+  </>
+)
--- packages/components/lib/index.ts
+++ packages/components/lib/index.ts
@@ -1,3 +1,4 @@
 export * from './Button'
+export * from './EmptyState'
 export * from './SelectInput'
 export * from './TextInput'
