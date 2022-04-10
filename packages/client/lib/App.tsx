9af2282126d8020e173916a84f02e60c2b550460
Niels Mokkenstorm <niels@mokkenstorm.dev>
2022-04-10 17:18:48 +0200

    fix: remove duplicate shadow

--- packages/client/lib/App.tsx
+++ packages/client/lib/App.tsx
@@ -16,15 +16,11 @@ export const App = () => (
         Graviton
       </h2>
     </div>
-      <div className="mt-8 sm:mx-auto sm:w-full md:max-w-3xl">
-      <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <Router>
-          <Routes>
-            <Route path="/" element={<Builder />} />
-            <Route path="/config" element={<Config />} />
-          </Routes>
-        </Router>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Builder />} />
+        <Route path="/config" element={<Config />} />
+      </Routes>
+    </Router>
   </div>
 )
